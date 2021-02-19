bae5e1fe1c41d9824178a02a80357224ef223f5a
sunabove <sunabove@nate.com>
2021-02-20 07:12:55 +0900

    **

--- src/web/FibonacciLoop.java
+++ src/web/FibonacciLoop.java
@@ -6,21 +6,20 @@ public class FibonacciLoop {
 	public static void main(String[] args) {
 		var out = System.out;
 		
-		int f0 = 1;
-		int f1 = 1;
+		int f0 = 0 ;
+		int f1 = 1 ;
 		int f;
 		
 		int lineNo = 1; 
-		while( f0 <= f1 ) {
+		do {
+			out.println( "".format( "%d, %d", lineNo, f1 ) );
+			
 			f = f0 + f1 ; 
 			f0 = f1;
 			f1 = f;
-			if( f0 <= f1 ) { 
-				out.println( "".format( "%d, %d", lineNo, f1 ) );
-			}
 			
 			lineNo ++ ; 
-		}
+		} while( f0 <= f1 ) ; 
 	}
 
 }
