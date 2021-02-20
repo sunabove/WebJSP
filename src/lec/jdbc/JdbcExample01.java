2d6b7432573eaadf735632ea85d6e464ffea3a91
sunabove <sunabove@nate.com>
2021-02-20 10:47:00 +0900

    **

--- src/lec/jdbc/JdbcExample01.java
+++ src/lec/jdbc/JdbcExample01.java
@@ -1,22 +1,8 @@
 package lec.jdbc;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.util.Properties;
-
-import javax.mail.Address;
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.NoSuchProviderException;
-import javax.mail.Session;
-import javax.mail.Store;
-import javax.mail.internet.InternetAddress;
-
-import lec.SysProps;
 
 public class JdbcExample01 {
 
